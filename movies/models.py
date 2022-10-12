ca385d1df37f94c1cc2882fec1bf1c1695a77bcc
daipham <daipham1210@gmail.com>
2022-10-12 20:41:07 +0700

    feat: change field movie description to not require

--- movies/models.py
+++ movies/models.py
@@ -4,7 +4,7 @@ from user.models import User
 class Movie(models.Model):
     url = models.CharField(max_length=512, default="")
     title = models.CharField(max_length=255, default="", blank=True)
-    description = models.TextField(default="")
+    description = models.TextField(default="", blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
