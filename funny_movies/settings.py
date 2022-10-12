1a0541229c066dd16b03bcb009430d0b4d2921a5
daipham <daipham1210@gmail.com>
2022-10-12 19:39:24 +0700

    feat: add csrf_trusted_origin

--- funny_movies/settings.py
+++ funny_movies/settings.py
@@ -133,4 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = ['https://funny-movies-remi.herokuapp.com/']
 django_heroku.settings(locals())
