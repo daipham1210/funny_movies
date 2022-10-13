ead9d6e4b66181eb81daec9eefb275f31ada671d
daipham <daipham1210@gmail.com>
2022-10-13 20:47:30 +0700

    feat: add unit test function generate iframe ytb

---
+++ movies/services.py
@@ -0,0 +1,39 @@
+from urllib.parse import urlparse, parse_qs
+
+class MovieService:
+    @classmethod
+    def get_video_id(cls, url):
+        """
+        Get video id from Youtube Link
+        Examples:
+        - http://youtu.be/SA2iWivDJiE
+        - http://www.youtube.com/watch?v=SA2iWivDJiE&feature=feedu
+        - http://www.youtube.com/embed/SA2iWivDJiE
+        - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+        return: SA2iWivDJiE
+        """
+        if url:
+            query = urlparse(url)
+            if query.hostname == 'youtu.be':
+                return query.path[1:]
+            if query.hostname in ('www.youtube.com', 'youtube.com'):
+                if query.path == '/watch':
+                    p = parse_qs(query.query)
+                    return p['v'][0]
+                if query.path[:7] == '/embed/':
+                    return query.path.split('/')[2]
+                if query.path[:3] == '/v/':
+                    return query.path.split('/')[2]
+        return None
+
+    @classmethod
+    def generate_iframe_ytb(cls, url, **kwargs):
+        """
+        Genenrate iframe video to show in HTML from Youtube URL
+        """
+        width = kwargs.get("width", 560)
+        height = kwargs.get("height", 315)
+        video_id = cls.get_video_id(url)
+        if video_id:
+            return f'<iframe width="{width}" height="{height}" src="//www.youtube.com/embed/{video_id}" frameborder="0" allowfullscreen></iframe>'
+        return ""
--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,36 @@
+from turtle import width
 from django.test import TestCase
 
-# Create your tests here.
+from movies.services import MovieService
+
+class MovieServiceTestCase(TestCase):
+    """
+    Test function generate_iframe_ytb (Unit Test)
+    """
+    def test_generate_iframe_ytb_success(self):
+        """
+        Input:
+            url: http://youtu.be/SA2iWivDJiE
+
+            width: 500
+            height: 300
+        Output:
+            '<iframe width="500" height="300" src="//www.youtube.com/embed/SA2iWivDJiE" frameborder="0" allowfullscreen></iframe>'
+        """
+        url = "http://youtu.be/SA2iWivDJiE"
+        expected_iframe = '<iframe width="500" height="300" src="//www.youtube.com/embed/SA2iWivDJiE" frameborder="0" allowfullscreen></iframe>'
+        self.assertEqual(MovieService.generate_iframe_ytb(url, width=500, height=300), expected_iframe)
+        
+        url = "http://www.youtube.com/watch?v=SA2iWivDJiE&feature=feedu"
+        self.assertEqual(MovieService.generate_iframe_ytb(url, width=500, height=300), expected_iframe)
+
+        url = "http://www.youtube.com/embed/SA2iWivDJiE"
+        self.assertEqual(MovieService.generate_iframe_ytb(url, width=500, height=300), expected_iframe)
+
+        url = "http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US"
+        self.assertEqual(MovieService.generate_iframe_ytb(url, width=500, height=300), expected_iframe)
+
+    def test_generate_iframe_ytb_with_wrong_url(self):
+        url = "http://youtube.com"
+        expected_iframe = ""
+        self.assertEqual(MovieService.generate_iframe_ytb(url), expected_iframe)
