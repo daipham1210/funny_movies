9dabf9aa1b0ecb7a0833b8a22ee6862a67f971c2
daipham <daipham1210@gmail.com>
2022-10-15 10:58:24 +0700

    feat: add validate email and password when login & register

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+import itertools
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import get_user_model
@@ -5,11 +6,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.http.response import JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from rest_framework import status
+from .forms import LoginForm
 
 UserModel = get_user_model()
 
 # Create your views here.
 class LoginOrSignup(LoginView):
+    form_class = LoginForm
     http_method_names = ['post']
 
     def _login(self, request, user):
@@ -17,18 +20,24 @@ class LoginOrSignup(LoginView):
         messages.add_message(request, messages.INFO, '{0} logged in.'.format(user.email))
         return JsonResponse(dict(success=True, user=dict(email=user.email)), safe=False, status=status.HTTP_200_OK)
     
+    def _response_error(self, messages, status_code):
+        return JsonResponse(dict(success=False, messages=messages), safe=False, status=status_code)
+
     def _response_invalid_email_or_password(self, request):
-        msg = "Invalid email or password"
-        return JsonResponse(dict(success=False, message=msg), safe=False, status=status.HTTP_401_UNAUTHORIZED)
+        return self._response_error(["Invalid email or password"], status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request, *args, **kwargs):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        if email and password:
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
             user = authenticate(email=email, password=password)
             if user:
                 return self._login(request, user)
-            return self._response_invalid_email_or_password(request)
+            return self._response_invalid_email_or_password(request)  
+        else:
+            error_messages = list(itertools.chain(*list(form.errors.values())))
+            return self._response_error(error_messages, status.HTTP_400_BAD_REQUEST)
 
 class Logout(LogoutView):
     http_method_names = ['get']
